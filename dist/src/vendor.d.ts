6967e6e41b80a1a9755141a8191e3ec9b7968866
patternfly-build <patternfly-build@redhat.com>
2017-05-25 14:17:45 +0000

    Added files generated by Travis build

---
+++ dist/src/vendor.d.ts
@@ -0,0 +1,9 @@
+import '@angular/common';
+import '@angular/core';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+import 'rxjs';
+import 'patternfly/dist/css/patternfly.css';
