b78668312aea5c2abcd3b232302cc0148aba0002
Dan <dlabrecq@redhat.com>
2017-06-26 16:17:29 -0400

    Implement all Action and ActionConfig properties for components that use these types

--- src/app/models/action-config.ts
+++ src/app/models/action-config.ts
@@ -4,9 +4,9 @@ import { Action } from './action';
  * An actions config containing:
  *
  * moreActions - Optional list of secondary kebab actions
- * moreActionsDisabledFn - Set to true to disable secondary actions kebab (list-view only)
- * moreActionsStyleClassFn - Optional style class for secondary actions kebab (list-view only)
- * moreActionsVisibleFn - Set to false to hide secondary actions kebab (list-view only)
+ * moreActionsDisabled - Set to true to disable secondary actions kebab
+ * moreActionsStyleClass - Optional style class for secondary actions kebab
+ * moreActionsVisible - Set to false to hide secondary actions kebab
  * primaryActions - List of primary button actions
  */
 export class ActionConfig {
