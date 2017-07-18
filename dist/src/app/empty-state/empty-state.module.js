dbe4b6fb84ad143f629b761a62096b821e8a3f9b
patternfly-build <patternfly-build@redhat.com>
2017-07-18 14:03:30 +0000

    Added files generated by Travis build

---
+++ dist/src/app/empty-state/empty-state.module.js
@@ -0,0 +1,28 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmptyStateConfig } from './empty-state-config';
+import { EmptyStateComponent } from './empty-state.component';
+export { EmptyStateConfig };
+/**
+ * A module containing objects associated with the empty state component
+ */
+var EmptyStateModule = (function () {
+    function EmptyStateModule() {
+    }
+    return EmptyStateModule;
+}());
+EmptyStateModule = __decorate([
+    NgModule({
+        imports: [CommonModule],
+        declarations: [EmptyStateComponent],
+        exports: [EmptyStateComponent]
+    })
+], EmptyStateModule);
+export { EmptyStateModule };
+//# sourceMappingURL=empty-state.module.js.map
