bc2e02f17781980f8fcb867c7dff5b45a2182ac2
patternfly-build <patternfly-build@redhat.com>
2017-08-06 05:36:30 +0000

    Added files generated by Travis build

---
+++ dist/src/app/filter/filter.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing filter properties
+ */
+var Filter = (function () {
+    function Filter() {
+    }
+    return Filter;
+}());
+export { Filter };
+//# sourceMappingURL=filter.js.map
