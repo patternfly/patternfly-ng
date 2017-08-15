59d2e62af13b324095a85412249ef06f5e7cbe10
Dana Gutride <dgutride@redhat.com>
2017-08-15 10:11:10 -0400

    feat(sparkline): add sparkline chart files

---
+++ src/app/chart/chart.base.ts
@@ -0,0 +1,45 @@
+import { Input, Output, EventEmitter } from '@angular/core';
+import * as c3 from 'c3';
+import { cloneDeep } from 'lodash';
+import { ChartConfig } from './chart-config';
+
+export abstract class ChartBase {
+
+  /**
+   * Chart configuration object with data
+   */
+  @Input() public config: ChartConfig;
+
+  /**
+   * Event emitted with the chart reference after load is complete
+   * @type {EventEmitter}
+   */
+  @Output() chartLoaded: EventEmitter<any> = new EventEmitter();
+
+  public prevConfig: any;
+  // store the chart object
+  private chart: any;
+
+  constructor() {
+  }
+
+  generateChart (chartId: string, reload?: boolean): void {
+    setTimeout(() => {
+      let c3Config = this.config;
+      if (c3Config) {
+        c3Config.bindto = '#' + chartId;
+        // always re-generate donut pct chart because it's colors
+        // change based on data and thresholds
+        if (!this.chart || reload || chartId.indexOf('donutPctChart') > -1) {
+          this.chart = c3.generate(c3Config);
+        } else {
+          //if chart is already created, then we only need to re-load data
+          this.chart.load(this.config.data);
+        }
+
+        this.chartLoaded.emit(this.chart);
+        this.prevConfig = cloneDeep(this.config);
+      }
+    });
+  }
+}
