3b4fb1419a431eb6519acf4046196059e69450ba
patternfly-build <patternfly-build@redhat.com>
2017-08-30 15:59:28 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/notification/notification-type.d.ts
@@ -0,0 +1,18 @@
+export declare class NotificationType {
+    /**
+     * Danger notification type
+     */
+    static readonly DANGER: string;
+    /**
+     * Information notification type
+     */
+    static readonly INFO: string;
+    /**
+     * Success notification type
+     */
+    static readonly SUCCESS: string;
+    /**
+     * Warning notification type
+     */
+    static readonly WARNING: string;
+}
