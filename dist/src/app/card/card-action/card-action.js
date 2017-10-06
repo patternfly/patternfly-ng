e258ec3b2850372f4f82cf1748c3fd9a69fd0ddb
patternfly-build <patternfly-build@redhat.com>
2017-10-06 18:14:27 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/card/card-action/card-action.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing card action properties
+ */
+var CardAction = (function () {
+    function CardAction() {
+    }
+    return CardAction;
+}());
+export { CardAction };
+//# sourceMappingURL=card-action.js.map
---
+++ dist/src/app/chart/chart.defaults.d.ts
@@ -0,0 +1,19 @@
+export declare class ChartDefaults {
+    private patternflyDefaults;
+    getDefaultColors: any;
+    getDefaultDonut: any;
+    getDefaultDonutSize: any;
+    getDefaultDonutColor: any;
+    getDefaultDonutLegend: any;
+    getDefaultDonutConfig: any;
+    getDefaultSparklineArea: any;
+    getDefaultSparklineSize: any;
+    getDefaultSparklineAxis: any;
+    getDefaultSparklineColor: any;
+    getDefaultSparklineLegend: any;
+    getDefaultSparklinePoint: any;
+    getDefaultSparklineTooltip: any;
+    getDefaultSparklineConfig: any;
+    getDefaultLineConfig: any;
+    constructor();
+}
