0b7fa235901c751b6f1c2ccfedd288641c080535
patternfly-build <patternfly-build@redhat.com>
2017-10-16 19:16:42 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/list/tree-list/tree-list-config.js
@@ -0,0 +1,23 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import { ListBaseConfig } from '../list-base-config';
+/**
+ * A config containing properties for tree list
+ */
+var TreeListConfig = (function (_super) {
+    __extends(TreeListConfig, _super);
+    function TreeListConfig() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return TreeListConfig;
+}(ListBaseConfig));
+export { TreeListConfig };
+//# sourceMappingURL=tree-list-config.js.map
---
+++ dist/src/app/navigation/navigation-item-config.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for navigation items
+ */
+var NavigationItemConfig = (function () {
+    function NavigationItemConfig() {
+    }
+    return NavigationItemConfig;
+}());
+export { NavigationItemConfig };
+//# sourceMappingURL=navigation-item-config.js.map
