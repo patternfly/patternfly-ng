aafdb4290fc9f9cfeec14e1575b0a89752f85fb2
patternfly-build <patternfly-build@redhat.com>
2017-10-16 17:23:47 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/toolbar/toolbar-config.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for toolbar
+ */
+var ToolbarConfig = (function () {
+    function ToolbarConfig() {
+    }
+    return ToolbarConfig;
+}());
+export { ToolbarConfig };
+//# sourceMappingURL=toolbar-config.js.map
