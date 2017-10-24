e41c0b3a993a116f758f58867497a159329b6405
patternfly-build <patternfly-build@redhat.com>
2017-10-24 14:49:42 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/pagination/pagination.module.d.ts
@@ -0,0 +1,7 @@
+import { PaginationConfig } from './pagination-config';
+export { PaginationConfig };
+/**
+ * A module containing objects associated with notification components
+ */
+export declare class PaginationModule {
+}
