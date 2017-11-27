70acee1db6873a55d5a193e45b68c23ef589bee4
patternfly-build <patternfly-build@redhat.com>
2017-11-27 19:05:15 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/wizard/wizard-step.js
@@ -0,0 +1,10 @@
+/**
+ * Wizard step
+ */
+var WizardStep = (function () {
+    function WizardStep() {
+    }
+    return WizardStep;
+}());
+export { WizardStep };
+//# sourceMappingURL=wizard-step.js.map
