24a2dbae8fb6685a0a409f2961acb0f65b0bbd03
patternfly-build <patternfly-build@redhat.com>
2017-11-27 21:01:08 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/modal/about-modal-event.d.ts
@@ -0,0 +1,9 @@
+/**
+ * An object containing events for pagination events
+ */
+export declare class AboutModalEvent {
+    /**
+     * Flag indicating Modal is open
+     */
+    close?: boolean;
+}
