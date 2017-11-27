6f759d83e919934392d1ab78e7d403de041cfef8
Jie Kang <jkang@redhat.com>
2017-11-27 10:09:58 -0500

    refactor(donut): update donut chart example

--- src/app/chart/donut/examples/donut-example.component.ts
+++ src/app/chart/donut/examples/donut-example.component.ts
@@ -4,6 +4,8 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 
+import { cloneDeep } from 'lodash';
+
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'donut-example',
@@ -13,25 +15,44 @@ export class DonutExampleComponent implements OnInit {
   public config: any = {
     chartId: 'exampleDonut',
     colors: {
-      Cats: '#0088ce',
-      Hamsters: '#3f9c35'
+      Cats: '#0088ce',     // blue
+      Hamsters: '#3f9c35', // green
+      Fish: '#ec7a08',     // orange
+      Dogs: '#cc0000'      // red
     },
     onClickFn: (data: any, element: any) => {
-      alert('Clicked!');
+      alert('You clicked on donut arc: ' + data.id);
     },
     donut: {
       title: 'Animals'
+    },
+    legend: {
+      show: true
     }
   };
+
+  public config2: any;
+
   public data: any = [
     ['Cats', 2],
-    ['Hamsters', 2]
+    ['Hamsters', 1],
+    ['Fish', 3],
+    ['Dogs', 2]
   ];
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.config.chartHeight = 180;
+    this.config2 = cloneDeep(this.config);
+    this.config2.chartId = 'exampleDonut2';
+    this.config2.legend = {
+      show: true,
+      position: 'right'
+    };
+    this.config2.centerLabelFn = () => {
+      return 'Pets';
+    };
+    this.config2.chartHeight = 120;
   }
 }
