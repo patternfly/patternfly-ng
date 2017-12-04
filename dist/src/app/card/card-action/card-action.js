6f00dfbf0b98959443682ee3953b476191e60bfc
patternfly-build <patternfly-build@redhat.com>
2017-12-04 20:15:14 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/card/card-action/card-action.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing card action properties
+ */
+var CardAction = /** @class */ (function () {
+    function CardAction() {
+    }
+    return CardAction;
+}());
+export { CardAction };
+//# sourceMappingURL=card-action.js.map
