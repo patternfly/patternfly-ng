97fe646f664666460a58a8e0026d90af0ed98f85
patternfly-build <patternfly-build@redhat.com>
2017-12-12 18:50:35 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/notification/notification.module.d.ts
@@ -0,0 +1,8 @@
+import { NotificationEvent } from './notification-event';
+import { NotificationType } from './notification-type';
+export { NotificationEvent, NotificationType };
+/**
+ * A module containing objects associated with notification components
+ */
+export declare class NotificationModule {
+}
---
+++ dist/src/app/toolbar/toolbar-view.js
@@ -0,0 +1,10 @@
+/**
+ * An view containing common properties
+ */
+var ToolbarView = /** @class */ (function () {
+    function ToolbarView() {
+    }
+    return ToolbarView;
+}());
+export { ToolbarView };
+//# sourceMappingURL=toolbar-view.js.map
