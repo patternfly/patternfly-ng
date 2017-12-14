e495853715af45fcc30524e49eb8b024152c83c4
patternfly-build <patternfly-build@redhat.com>
2017-12-14 21:15:00 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/navigation/navigation-item-config.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for navigation items
+ */
+var NavigationItemConfig = /** @class */ (function () {
+    function NavigationItemConfig() {
+    }
+    return NavigationItemConfig;
+}());
+export { NavigationItemConfig };
+//# sourceMappingURL=navigation-item-config.js.map
