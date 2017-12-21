3958f305be15c47ec6360cfe6f245e5605220880
patternfly-build <patternfly-build@redhat.com>
2017-12-21 15:17:23 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/action/action.js
@@ -0,0 +1,10 @@
+/**
+ * An action containing common properties for buttons, kebabs, etc.
+ */
+var Action = /** @class */ (function () {
+    function Action() {
+    }
+    return Action;
+}());
+export { Action };
+//# sourceMappingURL=action.js.map
