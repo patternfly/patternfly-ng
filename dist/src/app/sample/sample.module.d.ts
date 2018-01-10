e77407ecd6185306de6845f95a79a484fb4529c5
patternfly-build <patternfly-build@redhat.com>
2018-01-10 16:09:59 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/sample/sample.module.d.ts
@@ -0,0 +1,5 @@
+/**
+ * A module containing objects associated with the sample component
+ */
+export declare class SampleModule {
+}
---
+++ dist/src/app/sort/sort-config.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for sort
+ */
+var SortConfig = /** @class */ (function () {
+    function SortConfig() {
+    }
+    return SortConfig;
+}());
+export { SortConfig };
+//# sourceMappingURL=sort-config.js.map
