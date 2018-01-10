57a7daefaa597b1fb75ed41829cde4485dc9bb9e
patternfly-build <patternfly-build@redhat.com>
2018-01-10 16:08:06 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/card/card-base-config.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for cards
+ */
+var CardBaseConfig = /** @class */ (function () {
+    function CardBaseConfig() {
+    }
+    return CardBaseConfig;
+}());
+export { CardBaseConfig };
+//# sourceMappingURL=card-base-config.js.map
---
+++ dist/src/app/card/card.module.d.ts
@@ -0,0 +1,13 @@
+import { CardAction } from './card-action/card-action';
+import { CardBase } from './card-base';
+import { CardBaseConfig } from './card-base-config';
+import { CardConfig } from './basic-card/card-config';
+import { CardFilter } from './card-filter/card-filter';
+import { CardFilterPosition } from './card-filter/card-filter-position';
+import { InfoStatusCardConfig } from './info-status-card/info-status-card-config';
+export { CardAction, CardBase, CardBaseConfig, CardConfig, CardFilter, CardFilterPosition, InfoStatusCardConfig };
+/**
+ * A module containing objects associated with card components
+ */
+export declare class CardModule {
+}
