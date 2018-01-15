15d4e2e0e00848d924385138b5c0202c403f2091
patternfly-build <patternfly-build@redhat.com>
2018-01-15 14:42:59 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/sort/sort-field.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing properties for a sortable field, used to select categories of sorting
+ */
+var SortField = /** @class */ (function () {
+    function SortField() {
+    }
+    return SortField;
+}());
+export { SortField };
+//# sourceMappingURL=sort-field.js.map
