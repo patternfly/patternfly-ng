6e0d133e2cbcda6aa949dcef258c628b21f790fa
patternfly-build <patternfly-build@redhat.com>
2018-01-19 15:10:44 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/table/table-event.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing properties for table events
+ */
+var TableEvent = /** @class */ (function () {
+    function TableEvent() {
+    }
+    return TableEvent;
+}());
+export { TableEvent };
+//# sourceMappingURL=table-event.js.map
