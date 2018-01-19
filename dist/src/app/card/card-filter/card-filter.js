0974b44b7843fb1fbbfda660618f570c9107473a
patternfly-build <patternfly-build@redhat.com>
2018-01-19 14:33:26 +0000

    chore(release): updates generated by Travis build

---
+++ dist/src/app/card/card-filter/card-filter.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing card filter properties
+ */
+var CardFilter = /** @class */ (function () {
+    function CardFilter() {
+    }
+    return CardFilter;
+}());
+export { CardFilter };
+//# sourceMappingURL=card-filter.js.map
---
+++ dist/src/app/filter/filter-field.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing properties for a filterable field, used to select categories of filters
+ */
+var FilterField = /** @class */ (function () {
+    function FilterField() {
+    }
+    return FilterField;
+}());
+export { FilterField };
+//# sourceMappingURL=filter-field.js.map
