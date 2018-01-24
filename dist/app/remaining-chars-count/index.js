270602b9845e54426b8abee219c088262ab78da8
patternfly-build <patternfly-build@redhat.com>
2018-01-24 00:27:08 +0000

    chore(release): updates generated by Travis build

---
+++ dist/app/remaining-chars-count/index.js
@@ -0,0 +1,3 @@
+export { RemainingCharsCountDirective } from './remaining-chars-count.directive';
+export { RemainingCharsCountModule } from './remaining-chars-count.module';
+//# sourceMappingURL=index.js.map
---
+++ dist/app/wizard/wizard-config.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for wizard
+ */
+var WizardConfig = /** @class */ (function () {
+    function WizardConfig() {
+    }
+    return WizardConfig;
+}());
+export { WizardConfig };
+//# sourceMappingURL=wizard-config.js.map
