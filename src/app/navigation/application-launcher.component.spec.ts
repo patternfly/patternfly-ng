e68dcb9b48c8ea582d308ff1f910ac11951bd739
Abhishek Koserwal <akoserwa@redhat.com>
2018-01-25 17:41:39 +0530

    added app launcher spec and less file

---
+++ src/app/navigation/application-launcher.component.spec.ts
@@ -0,0 +1,11 @@
+import {
+    async,
+    ComponentFixture,
+    TestBed
+  } from '@angular/core/testing';
+  import { By } from '@angular/platform-browser';
+  import { FormsModule } from '@angular/forms';
+  
+  import { NavigationItemConfig } from './navigation-item-config';
+  import { TooltipModule } from 'ngx-bootstrap';
+  import { ApplicationLauncherComponent } from './application-launcher.component';
