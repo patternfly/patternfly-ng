5e8c281c68fb9a002fa479bd820853caf78d1a8a
patternfly-build <patternfly-build@redhat.com>
2018-01-25 21:49:43 +0000

    chore(release): updates generated by Travis build

---
+++ dist/app/card/card-filter/card-filter-position.js
@@ -0,0 +1,18 @@
+/*
+ * An object containing filter positions
+ */
+var CardFilterPosition = /** @class */ (function () {
+    function CardFilterPosition() {
+    }
+    /**
+     * Footer position
+     */
+    CardFilterPosition.FOOTER = 'footer';
+    /**
+     * Header position
+     */
+    CardFilterPosition.HEADER = 'header';
+    return CardFilterPosition;
+}());
+export { CardFilterPosition };
+//# sourceMappingURL=card-filter-position.js.map
---
+++ dist/app/notification/index.d.ts
@@ -0,0 +1,8 @@
+export { InlineNotificationComponent } from './inline-notification/inline-notification.component';
+export { Notification } from './notification';
+export { NotificationEvent } from './notification-event';
+export { NotificationModule } from './notification.module';
+export { NotificationType } from './notification-type';
+export { NotificationService } from './notification-service/notification.service';
+export { ToastNotificationComponent } from './toast-notification/toast-notification.component';
+export { ToastNotificationListComponent } from './toast-notification-list/toast-notification-list.component';
