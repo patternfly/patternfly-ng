6104dccc0a1b407855df92ce0a1553432accf3ef
Abhishek Koserwal <akoserwa@redhat.com>
2018-01-29 15:59:14 +0530

    added demo for app launcher

---
+++ src/app/navigation/example/application-launcher-example.component.ts
@@ -0,0 +1,71 @@
+import {
+    Component, ElementRef, EventEmitter,
+    Input, OnInit, Output, Renderer2,
+    TemplateRef, ViewEncapsulation
+} from '@angular/core';
+
+import { NavigationItemConfig } from '../navigation-item-config';
+
+@Component({
+    encapsulation: ViewEncapsulation.None,
+    selector: 'application-launcher-example',
+    templateUrl: './application-launcher-example.component.html'
+})
+
+export class ApplicationLauncherExampleComponent  implements OnInit {
+
+
+  navigationItems: NavigationItemConfig[];
+
+  ngOnInit(): void {
+    this.navigationItems = [
+      {
+        title: 'Recteque',
+        url : '#/applauncher/recteque',
+        iconStyleClass: 'pficon-storage-domain',
+        badges: [{
+                  count: 1,
+                  tooltip: 'Launch the Function User Interface'
+
+        }]
+      },
+      {
+        title: 'Suavitate',
+        url : '#/applauncher/intellegam/suavitate',
+        iconStyleClass: 'pficon-build',
+        badges: [{
+                count: 2,
+                tooltip: 'Launch the Function User Interface'
+        }]
+      },
+      {
+        title: 'Lorem',
+        url : '#/applauncher/intellegam/lorem',
+        iconStyleClass: 'pficon-domain',
+        badges: [{
+                count: 3,
+                tooltip: 'Launch the Function User Interface'
+        }]
+      },
+      {
+        title: 'Home',
+        url : '/',
+        iconStyleClass: 'pficon-home',
+        badges: [{
+                count: 4,
+                tooltip: 'Launch the Function User Interface'
+        }]
+      }
+    ];
+
+  }
+    /**
+     * The default constructor
+     */
+    constructor() {}
+
+
+
+
+}
+
