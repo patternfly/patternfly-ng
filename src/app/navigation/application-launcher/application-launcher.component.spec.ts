87eb210b78b52124f6c04e5a0af777d77451652b
Dan Labrecque <dlabrecq@redhat.com>
2018-01-31 16:09:41 -0500

    feat(app-launcher): add application launcher
    This cleans up some formatting, removes unused classes, fixes duplicate class attributes, and removes uib-dropdown and uib-keyboard-nav attributes leftover from angular-bootstrap — using ngx-bootstrap dropdown instead. Also adding the 'feat' commit comment here as it was missing -- a new feature release was never created.

--- src/app/navigation/application-launcher/application-launcher.component.spec.ts
+++ src/app/navigation/application-launcher/application-launcher.component.spec.ts
@@ -1,123 +1,112 @@
 import {
-    async,
-    ComponentFixture,
-    TestBed
-  } from '@angular/core/testing';
-  import { By } from '@angular/platform-browser';
-  import { FormsModule } from '@angular/forms';
-  import { RouterTestingModule } from '@angular/router/testing';
-  import { NavigationItemConfig } from '../navigation-item-config';
-  import { TooltipModule } from 'ngx-bootstrap';
-  import { ApplicationLauncherComponent } from './application-launcher.component';
+  async,
+  fakeAsync,
+  tick,
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
+import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap';
 
-  describe('Application Launcher componet', () => {
-    let comp: ApplicationLauncherComponent;
-    let fixture: ComponentFixture<ApplicationLauncherComponent>;
-    let navigationItems: NavigationItemConfig[];
-    let isClicked: boolean = false;
+import { ApplicationLauncherComponent } from './application-launcher.component';
+import { NavigationItemConfig } from '../navigation-item-config';
 
+describe('Application Launcher componet', () => {
+  let comp: ApplicationLauncherComponent;
+  let fixture: ComponentFixture<ApplicationLauncherComponent>;
+  let navigationItems: NavigationItemConfig[];
 
-    beforeEach(() => {
-
-      navigationItems = [
-        {
-          title: 'Recteque',
-          url : '#/applauncher/recteque',
-          iconStyleClass: 'pficon-storage-domain',
-          badges: [{
-                    count: 1,
-                    tooltip: 'Launch the Function User Interface'
-
-          }]
-        },
-        {
-          title: 'Suavitate',
-          url : '#/applauncher/intellegam/suavitate',
-          iconStyleClass: 'pficon-build',
-          badges: [{
-                  count: 2,
-                  tooltip: 'Launch the Function User Interface'
-          }]
-        },
-        {
-          title: 'Lorem',
-          url : '#/applauncher/intellegam/lorem',
-          iconStyleClass: 'pficon-domain',
-          badges: [{
-                  count: 3,
-                  tooltip: 'Launch the Function User Interface'
-          }]
-        },
-        {
-          title: 'Home',
-          url : '/',
-          iconStyleClass: 'pficon-home',
-          badges: [{
-                  count: 4,
-                  tooltip: 'Launch the Function User Interface'
-          }]
-        }
-      ];
-
-    });
-
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        imports: [FormsModule, TooltipModule.forRoot(), RouterTestingModule],
-        declarations: [ApplicationLauncherComponent]
-      })
-        .compileComponents()
-        .then(() => {
-          fixture = TestBed.createComponent(ApplicationLauncherComponent);
-          comp = fixture.componentInstance;
-
-
-          comp.items = navigationItems;
-          comp.label = 'Application Launche';
-          comp.showAsList = false;
-          comp.hiddenIcons = false;
-          comp.disabled = false;
-
-          fixture.detectChanges();
-        });
-    }));
-
-    it('should add the applauncher nav ', () => {
-      let primaryMenu = fixture.debugElement.query(By.css('.applauncher-pf'));
-      expect(primaryMenu).not.toBeNull();
-
-    });
-
-    it('should not show icons in hiddenIcons mode', () => {
-
-      comp.hiddenIcons = true;
-      fixture.detectChanges();
-
-      let primaryItems = fixture.debugElement.queryAll(
-        By.css('.applauncher-pf-item'));
-        expect(primaryItems.length).toBe(4);
-
-
-      let iconSpan = primaryItems[0].query(By.css('.applauncher-pf-item > a > i'));
-      expect(iconSpan.classes['hidden']).toBeTruthy();
-    });
-
-
-    it('should invoke the toggle when an item is clicked', () => {
-      expect(comp.opened).toBeFalsy();
-
-      let primaryItems = fixture.debugElement.query(
-        By.css('.applauncher-pf > a'));
-        expect(primaryItems);
-
-      primaryItems.triggerEventHandler('click', {});
-      fixture.detectChanges();
-
-      expect(comp.opened).toBeTruthy();
-
-    });
-
-
+  beforeEach(() => {
+    navigationItems = [{
+      title: 'Recteque',
+      url : '#/applauncher',
+      iconStyleClass: 'pficon-storage-domain',
+      badges: [{
+        count: 1,
+        tooltip: 'Launch the Function User Interface'
+      }]
+    }, {
+      title: 'Suavitate',
+      url : '#/applauncher',
+      iconStyleClass: 'pficon-build',
+      badges: [{
+        count: 2,
+        tooltip: 'Launch the Function User Interface'
+      }]
+    }, {
+      title: 'Lorem',
+      url : '#/applauncher',
+      iconStyleClass: 'pficon-domain',
+      badges: [{
+        count: 3,
+        tooltip: 'Launch the Function User Interface'
+      }]
+    }, {
+      title: 'Home',
+      url : '/',
+      iconStyleClass: 'pficon-home',
+      badges: [{
+        count: 4,
+        tooltip: 'Launch the Function User Interface'
+      }]
+    }];
   });
 
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BsDropdownModule.forRoot(),
+        FormsModule,
+        TooltipModule.forRoot()
+      ],
+      declarations: [ApplicationLauncherComponent],
+      providers: [BsDropdownConfig]
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(ApplicationLauncherComponent);
+        comp = fixture.componentInstance;
+        comp.items = navigationItems;
+        comp.label = 'Application Launcher';
+        comp.showAsList = false;
+        comp.showIcons = true;
+        comp.disabled = false;
+        fixture.detectChanges();
+      });
+  }));
+
+  it('should add the applauncher nav ', () => {
+    let primaryMenu = fixture.debugElement.query(By.css('.applauncher-pf'));
+    expect(primaryMenu).not.toBeNull();
+  });
+
+  it('should not show icons', fakeAsync(() => {
+    comp.showIcons = false;
+    const element = fixture.nativeElement;
+
+    let button = element.querySelector('.applauncher-pf .dropdown-toggle');
+    button.click();
+    tick();
+    fixture.detectChanges(); // Workaround to fix dropdown tests
+
+    let primaryItems = fixture.debugElement.queryAll(By.css('.applauncher-pf-item'));
+    expect(primaryItems.length).toBe(4);
+
+    let iconSpan = primaryItems[0].query(By.css('.applauncher-pf-item > a > i'));
+    expect(iconSpan.classes['hidden']).toBeTruthy();
+  }));
+
+  it('should invoke the toggle when an item is clicked', () => {
+    let menu = fixture.debugElement.query(By.css('.applauncher-pf'));
+    expect(menu);
+
+    menu.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    menu = fixture.debugElement.query(By.css('.applauncher-pf .open'));
+    expect(menu);
+  });
+});
--- src/app/navigation/application-launcher/application-launcher.component.ts
+++ src/app/navigation/application-launcher/application-launcher.component.ts
@@ -1,89 +1,55 @@
 import {
-    Component, ElementRef, EventEmitter,
-    Input, OnInit, Output, Renderer2,
-    TemplateRef, ViewEncapsulation
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation
 } from '@angular/core';
 
 import { NavigationItemConfig } from '../navigation-item-config';
-import { read } from 'fs-extra';
 
 @Component({
-    encapsulation: ViewEncapsulation.None,
-    selector: 'pfng-application-launcher',
-    templateUrl: './application-launcher.component.html'
+  encapsulation: ViewEncapsulation.None,
+  selector: 'pfng-application-launcher',
+  templateUrl: './application-launcher.component.html'
 })
 
 /**
  * Application launcher component
  */
 export class ApplicationLauncherComponent  implements OnInit {
+  /**
+   * Disable the application launcher button, default: false
+   */
+  @Input() disabled: boolean;
 
-    /**
-     *  Use a custom label for the launcher, default: Application Launcher
-     */
-    @Input() label: string;
+  /**
+   * The navigation items used to build the menu
+   */
+  @Input() items: NavigationItemConfig[];
 
+  /**
+   *  Use a custom label for the launcher, default: Application Launcher
+   */
+  @Input() label: string;
 
-    /**
-     * Disable the application launcher button, default: false
-     */
-    @Input() disabled: boolean;
+  /**
+   * Display items as a list instead of a grid, default: false
+   */
+  @Input() showAsList: boolean = false;
 
+  /**
+   * Flag to show icons on the launcher, default: true
+   */
+  @Input() showIcons: boolean = true;
 
-    /**
-     * Display items as a list instead of a grid, default: false
-     */
-    @Input() showAsList: boolean;
+  /**
+   * The default constructor
+   */
+  constructor() {}
 
-
-    /**
-     * Flag to not show icons on the launcher, default: false
-     */
-    @Input() hiddenIcons: boolean;
-
-
-    /**
-     * The navigation items used to build the menu
-     */
-    @Input() items: NavigationItemConfig[];
-
-
-    /**
-     * Internal boolean to toggle launcher, default:false
-     */
-    private _opened: boolean;
-
-
-    /**
-     * The default constructor
-     */
-    constructor() {}
-
-    /**
-     * Initialize variable
-     */
-    ngOnInit(): void {
-      this._opened = false;
-    }
-
-    /**
-     * getter
-     */
-    get opened(): boolean {
-      return this._opened;
-      }
-
-    /**
-     * toggle function for launcher, active when click, return false on isDisabled:true
-     */
-   public toggle() {
-      if (this.disabled) {
-        return false;
-      } else {
-        this._opened = !this._opened;
-      }
-
-    }
-
-
+  /**
+   * Initialize variable
+   */
+  ngOnInit(): void {
+  }
 }
