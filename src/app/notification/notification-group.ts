f69e342935251fe7fdd461c63c574a35da2c12e7
Abhishek Koserwal <akoserwa@redhat.com>
2018-03-19 02:35:35 +0530

    added flag for collapse toggle

--- src/app/notification/notification-group.ts
+++ src/app/notification/notification-group.ts
@@ -1,27 +1,33 @@
 import { Notification } from './notification';
 import { TemplateRef } from '@angular/core';
+import { EmptyStateConfig } from '../empty-state/empty-state-config';
 /**
  * An object containing properties for notification groups
  */
 export class NotificaitonGroup {
 
+  
   /**
    * Include src for the heading area for each notification group,
    *  access the group via notificationGroup
    */
-  heading: TemplateRef<any>;
-
-
+  heading: string;
+  
+  
   /**
    * Include src for the sub-heading area for each notification group,
    * access the group via notificationGroup
    */
-  subHeading: TemplateRef<any>;
-
-
+  subHeading: string;
+  
+  
   /**
    * Configuration properties for a notification message
    */
   notification: Notification;
-
+  
+  /**
+   * 
+   */
+  open: boolean;
 }
