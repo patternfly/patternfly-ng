f7f7961766f888ba788997db073ef867bc02af28
patternfly-build <patternfly-build@redhat.com>
2018-05-21 15:37:43 +0000

    chore(release): updates generated by Travis build

---
+++ dist/app/notification/notification-group.js
@@ -0,0 +1,10 @@
+/**
+ * An object containing properties for notification groups
+ */
+var NotificaitonGroup = /** @class */ (function () {
+    function NotificaitonGroup() {
+    }
+    return NotificaitonGroup;
+}());
+export { NotificaitonGroup };
+//# sourceMappingURL=notification-group.js.map
