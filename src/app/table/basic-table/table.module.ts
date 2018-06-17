c75af0425cd71ae67e8ac822ed1dfcde04e1d8e2
Dan Labrecque <dlabrecq@redhat.com>
2018-06-16 22:48:35 -0400

    feat(package): individual module imports for table components (#389)
    This allows components to be imported individually without having to install an unused, optional dependency.

--- src/app/table/basic-table/table.module.ts
+++ src/app/table/basic-table/table.module.ts
@@ -5,22 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { DragulaModule, DragulaService } from 'ng2-dragula';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
-import { PaginationModule } from '../pagination/pagination.module';
-import { EmptyStateModule } from '../empty-state/empty-state.module';
-import { NgxDataTableConfig } from './basic-table/ngx-datatable-config';
-import { NgxDataTableDndDirective } from './basic-table/ngx-datatable-dnd.directive';
-import { TableBase } from './table-base';
-import { TableComponent } from './basic-table/table.component';
-import { TableConfig } from './basic-table/table-config';
-import { TableConfigBase } from './table-config-base';
-import { TableEvent } from './table-event';
-import { ToolbarModule } from '../toolbar/toolbar.module';
+import { PaginationModule } from '../../pagination/pagination.module';
+import { EmptyStateModule } from '../../empty-state/empty-state.module';
+import { NgxDataTableConfig } from './ngx-datatable-config';
+import { NgxDataTableDndDirective } from './ngx-datatable-dnd.directive';
+import { TableComponent } from './table.component';
+import { TableConfig } from './table-config';
+import { TableEvent } from '../table-event';
+import { ToolbarModule } from '../../toolbar/toolbar.module';
 
 export {
   NgxDataTableConfig,
-  TableBase,
   TableConfig,
-  TableConfigBase,
   TableEvent
 };
 
