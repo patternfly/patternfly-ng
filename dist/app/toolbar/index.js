abc3cbf8ec0e9063528efecdde7f3df7abafc74c
patternfly-build <patternfly-build@redhat.com>
2018-06-17 02:56:53 +0000

    chore(release): updates generated by Travis build

---
+++ dist/app/toolbar/index.js
@@ -0,0 +1,5 @@
+export { ToolbarConfig } from './toolbar-config';
+export { ToolbarComponent } from './toolbar.component';
+export { ToolbarModule } from './toolbar.module';
+export { ToolbarView } from './toolbar-view';
+//# sourceMappingURL=index.js.map
