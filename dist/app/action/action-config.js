a3e6eaac87f8275f7c14add204ea88f2dc1d8c6c
patternfly-build <patternfly-build@redhat.com>
2018-06-17 05:30:21 +0000

    chore(release): dist updates generated by Travis build

---
+++ dist/app/action/action-config.js
@@ -0,0 +1,11 @@
+/**
+ * An action config containing properties for primary and secondary actions such as
+ * multiple buttons and kebab menu options
+ */
+var ActionConfig = /** @class */ (function () {
+    function ActionConfig() {
+    }
+    return ActionConfig;
+}());
+export { ActionConfig };
+//# sourceMappingURL=action-config.js.map
---
+++ dist/app/modal/index.js
@@ -0,0 +1,4 @@
+export { AboutModalConfig } from './about-modal-config';
+export { AboutModalComponent } from './about-modal.component';
+export { ModalModule } from './modal.module';
+//# sourceMappingURL=index.js.map
