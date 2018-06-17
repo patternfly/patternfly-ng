599a3fe771656c3987dd48e769e50e4085f71d53
patternfly-build <patternfly-build@redhat.com>
2018-06-17 06:11:16 +0000

    chore(release): dist updates generated by Travis build

---
+++ dist/app/list/tree-list/tree-list.module.d.ts
@@ -0,0 +1,8 @@
+import { ListEvent } from '../list-event';
+import { TreeListConfig } from './tree-list-config';
+export { ListEvent, TreeListConfig };
+/**
+ * A module containing objects associated with tree list components
+ */
+export declare class TreeListModule {
+}
