cfe146f1ab2ac469d06e038f1c0a4ddfb17ea931
Dan Labrecque <dlabrecq@redhat.com>
2018-06-17 23:25:22 -0400

    fix(tree-list): deprecated for Angular 6 compatibility (#376)
    Deprecated the tree-list component due to issues with Angular 6 and mobx autorun, introduced by angular-tree-component.
    
    Fixes https://github.com/patternfly/patternfly-ng/issues/381

--- src/app/list/tree-list/tree-list-config.ts
+++ src/app/list/tree-list/tree-list-config.ts
@@ -2,6 +2,11 @@ import { ListConfigBase } from '../list-config-base';
 
 /**
  * A config containing properties for tree list
+ *
+ * @deprecated The tree-list component is deprecated due to issues with Angular 6 and mobx autorun,
+ * introduced by angular-tree-component.
+ *
+ * See: https://github.com/patternfly/patternfly-ng/issues/381
  */
 export class TreeListConfig extends ListConfigBase {
   /**
