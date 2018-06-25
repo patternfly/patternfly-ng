0d1207e79174ab3b0bda4a95493bb7c6e33a09fa
patternfly-build <patternfly-build@redhat.com>
2018-06-25 15:01:58 +0000

    chore(release): dist updates generated by Travis build

---
+++ dist/app/card/card-config-base.js
@@ -0,0 +1,10 @@
+/**
+ * A config containing properties for cards
+ */
+var CardConfigBase = /** @class */ (function () {
+    function CardConfigBase() {
+    }
+    return CardConfigBase;
+}());
+export { CardConfigBase };
+//# sourceMappingURL=card-config-base.js.map
---
+++ dist/app/chart/sparkline-chart/sparkline-chart-config.js
@@ -0,0 +1,36 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+import { ChartConfigBase } from '../chart-config-base';
+/**
+ * A config containing properties for the sparkline chart
+ */
+var SparklineChartConfig = /** @class */ (function (_super) {
+    __extends(SparklineChartConfig, _super);
+    function SparklineChartConfig() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return SparklineChartConfig;
+}(ChartConfigBase));
+export { SparklineChartConfig };
+/**
+ * @deprecated Use SparklineChartConfig
+ */
+var SparklineConfig = /** @class */ (function (_super) {
+    __extends(SparklineConfig, _super);
+    function SparklineConfig() {
+        var _this = _super.call(this) || this;
+        console.log('patternfly-ng: SparklineConfig is deprecated; use SparklineChartConfig');
+        return _this;
+    }
+    return SparklineConfig;
+}(SparklineChartConfig));
+export { SparklineConfig };
+//# sourceMappingURL=sparkline-chart-config.js.map
