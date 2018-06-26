fb2be29571ecdc2e7fc206dfa23570d10180cf88
patternfly-build <patternfly-build@redhat.com>
2018-06-26 23:18:47 +0000

    Added files from commit #2e60b39ec79255bde357cdbb2b550b5264a38092

--- src/app/copy/inline-copy/example/inline-copy-basic-example.component.ts
+++ src/app/copy/inline-copy/example/inline-copy-basic-example.component.ts
@@ -3,6 +3,7 @@ import {
   OnInit,
   ViewEncapsulation
 } from '@angular/core';
+import {CopyEvent} from '../../copy-event';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
@@ -10,7 +11,7 @@ import {
   templateUrl: './inline-copy-basic-example.component.html'
 })
 export class InlineCopyBasicExampleComponent implements OnInit {
-
+  actionsText: string = '';
   basicExConfig = {
     copyBtnAriaLabel: 'Copy WAI-ARIA URL',
     copyValue: 'https://www.w3.org/TR/wai-aria-1.1/',
@@ -20,4 +21,8 @@ export class InlineCopyBasicExampleComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  handleCopyToClipboard($event: CopyEvent): void {
+    this.actionsText = 'Copied: ' + $event.value + '\r\n' + this.actionsText;
+  }
 }
